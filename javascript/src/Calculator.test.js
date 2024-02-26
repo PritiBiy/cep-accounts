1490a6d51237fab5816aeb4da03e8ec05a1d357d
Priti <pritibiy@thoughtworks.com>
2024-02-26 17:46:47 +0800

    Added test for the calculator

---
+++ javascript/src/Calculator.test.js
@@ -0,0 +1,9 @@
+// const Calculator = require("./Calculator");
+
+describe('Calculator', () => {
+
+    it('add 1 and 2 to make 3', () => {
+        // expect(calculator.add(1,2)).toBe(3);
+    });
+
+});
